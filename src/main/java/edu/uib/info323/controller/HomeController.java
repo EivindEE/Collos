383bfd82ce1c051c6f4b9d42a6dce3e786cf1d91
EivindEE <eivind.elseth@gmail.com>
2012-04-11 11:48:52 +0200

    Added a ImageDaoImpl. Added a reference to the DAO in HomeController for mock purposes.

--- src/main/java/edu/uib/info323/controller/HomeController.java
+++ src/main/java/edu/uib/info323/controller/HomeController.java
@@ -7,14 +7,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import edu.uib.info323.dao.ImageDaoImpl;
+import edu.uib.info323.model.Image;
 
 /**
  * Sample controller for going to the home page with a message
@@ -24,7 +30,8 @@ public class HomeController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
-
+	@Autowired
+	private DataSource dataSource;
 	/**
 	 * Selects the home page and populates the model with a message
 	 */
@@ -32,11 +39,18 @@ public class HomeController {
 	public ModelAndView home() {
 		
 //		EmbeddedDatabase database = new EmbeddedDatabaseBuilder().addScript("schema.sql").addScript("test-data.sql").build();
-		List<String> urls = new LinkedList<String>();
+		ImageDaoImpl daoImpl = new ImageDaoImpl();
+		daoImpl.setDataSource(dataSource);
+
 		File imageFolder = new File("src/main/webapp/resources/testimg");
 		for(File f :imageFolder.listFiles()){
-			urls.add(f.getName());
+			Image img = new Image();
+			img.setImageUri(f.getName());
+			img.setPageUri("http://example.org/" + f.getName());
+			daoImpl.insert(img);
 		}
+
+		List<Image> urls = daoImpl.getAllImages();
 		LOGGER.error("Returning file list: " + urls);
 		Map<String, Object> model = new TreeMap<String, Object>();
 		model.put("images", urls);
