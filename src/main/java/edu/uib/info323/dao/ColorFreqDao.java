994d3dcefcfe6a31ec5be085c51adab8aedc64ae
EivindEE <eivind.elseth@gmail.com>
2012-04-18 13:13:36 +0200

    Added setDatasource() to ColorFreqDao

--- src/main/java/edu/uib/info323/dao/ColorFreqDao.java
+++ src/main/java/edu/uib/info323/dao/ColorFreqDao.java
@@ -1,5 +1,9 @@
 package edu.uib.info323.dao;
 
+import javax.sql.DataSource;
+
 public interface ColorFreqDao {
+
+	public void setDataSource(DataSource datasource);
 
 }
--- src/main/java/edu/uib/info323/dao/ColorFreqDaoImpl.java
+++ src/main/java/edu/uib/info323/dao/ColorFreqDaoImpl.java
@@ -1,5 +1,16 @@
 package edu.uib.info323.dao;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class ColorFreqDaoImpl implements ColorFreqDao {
+	private JdbcTemplate jdbcTemplate;
+	
+	public void setDataSource(DataSource datasource) {
+		this.jdbcTemplate = new JdbcTemplate(datasource);
+	}
+	
+	
 
 }
