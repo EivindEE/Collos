cf1b946313e373299fb169d4aeb5562f3b2db34d
EivindEE <eivind.elseth@gmail.com>
2012-04-19 13:32:23 +0200

    Moved testDB setup to new class DBSetup

--- src/main/java/edu/uib/info323/controller/HomeController.java
+++ src/main/java/edu/uib/info323/controller/HomeController.java
@@ -1,8 +1,6 @@
 package edu.uib.info323.controller;
 
 
-import java.io.File;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -13,8 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +21,6 @@ import org.springframework.web.servlet.ModelAndView;
 import edu.uib.info323.dao.ImageDao;
 import edu.uib.info323.dao.ImageDaoImpl;
 import edu.uib.info323.model.Image;
-import edu.uib.info323.model.ImageImpl;
 
 /**
  * Sample controller for going to the home page with a message
@@ -55,11 +50,9 @@ public class HomeController {
 	public ModelAndView serch(@RequestParam(required=false) String color) {
 		LOGGER.error("Returning color: " + color);
 
-		//		EmbeddedDatabase database = new EmbeddedDatabaseBuilder().addScript("schema.sql").addScript("test-data.sql").build();
 		ImageDao daoImpl = new ImageDaoImpl();
 		daoImpl.setDataSource(dataSource);
 		List<Image> urls = daoImpl.getAllImages();
-		//LOGGER.error("Returning file list: " + urls);
 		Map<String, Object> model = new TreeMap<String, Object>();
 		model.put("images", urls);
 		ModelAndView mav = new ModelAndView("home", model);
@@ -68,19 +61,8 @@ public class HomeController {
 
 	@RequestMapping("/spring")
 	public String redirectSpring() {
-		ImageDao daoImpl = new ImageDaoImpl();
-		daoImpl.setDataSource(dataSource);
-		
-		File imageFolder = new File("src/main/webapp/resources/testimg");
-		for(File f :imageFolder.listFiles()){
-			Image img = new ImageImpl("resources/testimg/" + f.getName(),"http://example.org/" + f.getName());
-			daoImpl.insert(img);
-		}
 		return "redirect:/search";
 	}
-//	public static void main(String[] args) {
-//		HomeController controller = new HomeController();
-//		controller.home();
-//	}
+
 
 }
