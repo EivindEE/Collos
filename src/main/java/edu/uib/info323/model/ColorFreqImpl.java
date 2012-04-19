e8dda0a0cd3140d38378bf83f94e479c583f0bc5
EivindEE <eivind.elseth@gmail.com>
2012-04-19 11:45:12 +0200

    Modified ColorFreqImpl equals(). Now they are equal if they have equal internal states.

--- src/main/java/edu/uib/info323/model/ColorFreqImpl.java
+++ src/main/java/edu/uib/info323/model/ColorFreqImpl.java
@@ -1,21 +1,25 @@
 package edu.uib.info323.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 
 public class ColorFreqImpl implements ColorFreq {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ColorFreqImpl.class);
 	private Image image;
 	private int color;
 	private int relativeFreq;
-	
+
 	public ColorFreqImpl() {
 	}
-	
+
 	public ColorFreqImpl(Image image, int color, int relativeFreq) {
 		this.image = image;
 		this.color = color;
 		this.relativeFreq = relativeFreq;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see edu.uib.info323.model.ColorFreq#getImage()
 	 */
@@ -49,25 +53,49 @@ public class ColorFreqImpl implements ColorFreq {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+//		LOGGER.debug("Comparing " + this + " to " + obj);
+		if (this == obj) {
+//			LOGGER.debug("TRUE: this == obj");
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
+//			LOGGER.debug("FALSE: obj == null");
 			return false;
-		if (!(obj instanceof ColorFreqImpl))
+		}
+		if (!(obj instanceof ColorFreq)) {
+//			LOGGER.debug("FALSE: !(" + obj + " instanceof ColorFreq)");
 			return false;
-		ColorFreqImpl other = (ColorFreqImpl) obj;
-		if (color != other.color)
+		}
+		ColorFreq other = (ColorFreq) obj;
+		if (color != other.getColor()) {
+//			LOGGER.debug("FALSE:" + this.color + " != " + other.getColor());
 			return false;
+		}
 		if (image == null) {
-			if (other.image != null)
+			if (other.getImage() != null) {
+//				LOGGER.debug("FALSE: One image is null");
 				return false;
-		} else if (!image.equals(other.image))
+			}
+		} else if (!image.equals(other.getImage())) {
+			LOGGER.debug("FALSE: !"+this.image + ".equals(" + other.getImage() +")");			
 			return false;
-		if (relativeFreq != other.relativeFreq)
+		}
+		if (relativeFreq != other.getRelativeFreq()) {
+//			LOGGER.debug("FALSE: " + this.relativeFreq + " != " + other.getRelativeFreq());
 			return false;
+		}
+//		LOGGER.debug("TRUE: "+ this +" equals "  + other);
 		return true;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "ColorFreqImpl [image=" + image + ", color=" + color
+				+ ", relativeFreq=" + relativeFreq + "]";
+	}
+
+
+
+
 
 }
