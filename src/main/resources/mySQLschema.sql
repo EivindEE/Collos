22b8f25b54b81a611294747ed26ada4796f31e3a
EivindEE <eivind.elseth@gmail.com>
2012-05-03 13:09:38 +0200

    Updated sql schema

--- src/main/resources/mySQLschema.sql
+++ src/main/resources/mySQLschema.sql
@@ -1,22 +1,22 @@
 CREATE TABLE image(
-  image_uri VARCHAR2(2000),
-  date_analyzed DATE,
-  suspended INT,
-  PRIMARY KEY(image_uri)
-  );
+	image_uri VARCHAR(500),
+	date_analyzed DATE,
+	suspended INT,
+	PRIMARY KEY(image_uri)
+);
 
 
-  CREATE TABLE image_page(
-  image_uri VARCHAR2(2000),
-  page_uri VARCHAR2(2000),
-	FOREIGN KEY (image_uri) REFERENCES image (image_uri),
-  PRIMARY KEY (image_uri , page_uri)
+CREATE TABLE image_page(
+	image_uri VARCHAR(500),
+	page_uri VARCHAR(500),
+	FOREIGN KEY (image_uri) REFERENCES image (image_uri) ON DELETE CASCADE ON UPDATE CASCADE ,
+	PRIMARY KEY (image_uri , page_uri)
 );
   
-  CREATE TABLE color(
-  image_uri VARCHAR2(2000),
-  color INT,
-  relative_freq INT,
-  FOREIGN KEY (image_uri) REFERENCES image (image_uri),
-  PRIMARY KEY(image_uri, color)
+CREATE TABLE color(
+	image_uri VARCHAR(500),
+	color INT,
+	relative_freq INT,
+	FOREIGN KEY (image_uri) REFERENCES image (image_uri) ON DELETE CASCADE ON UPDATE CASCADE ,
+	PRIMARY KEY(image_uri, color)
 );
