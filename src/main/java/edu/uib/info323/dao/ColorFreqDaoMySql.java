8131e377f04205343cc381abb2e7cdc6cad1f4be
EivindEE <eivind.elseth@gmail.com>
2012-05-03 12:44:44 +0200

    Commiting the MySQL dao implementations

---
+++ src/main/java/edu/uib/info323/dao/ColorFreqDaoMySql.java
@@ -0,0 +1,87 @@
+package edu.uib.info323.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import edu.uib.info323.dao.rowmapper.ColorFreqRowMapper;
+import edu.uib.info323.model.ColorFreq;
+import edu.uib.info323.model.ColorFreqImpl;
+import edu.uib.info323.model.Image;
+
+@Component
+public class ColorFreqDaoMySql implements ColorFreqDao {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ColorFreqDaoMySql.class);
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private ImageDao imageDao;
+
+	private ColorFreqRowMapper rowMapper;
+
+	@Autowired
+	public void setDataSource(DataSource datasource) {
+		jdbcTemplate = new JdbcTemplate(datasource);
+
+	}
+
+	public List<ColorFreq> getAllColors() {
+		String sql = "SELECT * FROM color";
+		return jdbcTemplate.query(sql, new RowMapper<ColorFreq>() {
+
+
+			public ColorFreq mapRow(ResultSet rs, int rowNum)
+					throws SQLException {
+				Image image = imageDao.getImageByImageUri(rs.getString("image_uri"));
+				return new ColorFreqImpl(image, rs.getInt("color"), rs.getInt("relative_freq"));
+			}
+			
+		});
+	}
+
+	public void insert(ColorFreq colorFreq) {
+		String sql = "INSERT INTO color(image_uri, color, relative_freq) VALUES (?,?,?)";
+		jdbcTemplate.update(sql, new Object[] {colorFreq.getImage().getImageUri(),colorFreq.getColor(),colorFreq.getRelativeFreq()});
+	}
+
+	public List<ColorFreq> getImageColorFreqs(Image image) {
+		String sql = "SELECT * FROM color WHERE image_uri = ?";
+		rowMapper.setImage(image);
+		List<ColorFreq> colorFreqs = jdbcTemplate.query(sql,new Object[] {image.getImageUri()}, rowMapper);
+		LOGGER.debug(colorFreqs.toString());
+		return colorFreqs;
+	}
+
+	public void batchInsert(final List<ColorFreq> colorList) {
+		String sql = "INSERT INTO color(image_uri, color, relative_freq) VALUES ( ?, ? ,?) ";
+		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+			
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				ColorFreq colorFreq = colorList.get(i);
+				ps.setString(1, colorFreq.getImage().getImageUri());
+				ps.setInt(2, colorFreq.getColor());
+				ps.setInt(3, colorFreq.getRelativeFreq());
+//				ps.setInt(4, colorFreq.getRelativeFreq());
+			}
+			
+			public int getBatchSize() {
+				return colorList.size();
+			}
+		});
+
+	}
+
+}
---
+++ src/main/java/edu/uib/info323/dao/ImageDaoMySql.java
@@ -0,0 +1,140 @@
+package edu.uib.info323.dao;
+
+import java.awt.Color;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import edu.uib.info323.image.CompressedColorFactory;
+import edu.uib.info323.model.Image;
+import edu.uib.info323.model.ImageImpl;
+
+@Component
+public class ImageDaoMySql implements ImageDao {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ImageDaoMySql.class);
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private CompressedColorFactory colorFactory;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public void insert(Image image) {
+		String sql = "INSERT INTO image (image_uri) VALUES (?) ON DUPLICATE KEY UPDATE image_uri = image_uri";
+		jdbcTemplate.update(sql, new Object[] {image.getImageUri()});
+
+		sql = "INSERT INTO image_page (image_uri,page_uri) VALUES (?,?) ON DUPLICATE KEY UPDATE image_uri = image_uri";
+		jdbcTemplate.update(sql, new Object[] {image.getImageUri(), image.getPageUri()});
+		
+		LOGGER.debug("Inserted " + image + " into database");
+
+	}
+
+	public Image getImageByImageUri(String imageUri) {
+		String sql = "SELECT image_uri, page_uri " +
+					 "FROM image_page " +
+					 "WHERE image_uri = ?";
+
+		return this.jdbcTemplate.query(sql, new Object[] {imageUri}, new ResultSetExtractor<Image>() {
+
+			public Image extractData(ResultSet rs) throws SQLException,
+			DataAccessException {
+				rs.next();
+				
+				return new ImageImpl(rs.getString("image_uri"), rs.getString("page_uri"));
+			}});
+	}
+
+	public List<Image> getAllImages() {
+		List<Image> images = new ArrayList<Image>();
+		String sql = "SELECT image_uri, page_uri FROM image_page";
+		images = this.jdbcTemplate.query(sql, new RowMapper<Image>() {
+
+			public Image mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new ImageImpl(rs.getString("image_uri"), rs.getString("page_uri"));
+			}
+
+		});
+		return images;
+	}
+
+	
+
+	public void batchInsert(final List<Image> imageList) {
+		String sql = "INSERT INTO image (image_uri) VALUES (?) ON DUPLICATE KEY UPDATE image_uri = image_uri";
+		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				Image image = imageList.get(i);
+				ps.setString(1, image.getImageUri());
+			}
+			public int getBatchSize() {
+				return imageList.size();
+			}
+		});
+
+		sql = "INSERT INTO image_page (image_uri,page_uri) VALUES (?,?) ON DUPLICATE KEY UPDATE image_uri = image_uri";
+		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				Image image = imageList.get(i);
+				ps.setString(1, image.getImageUri());
+				ps.setString(2, image.getPageUri());
+			}
+			public int getBatchSize() {
+				return imageList.size();
+			}
+		});
+
+	}
+
+	public List<Image> getUnprocessedImages() {
+		String sql = "SELECT * FROM image WHERE date_analyzed IS NULL";
+
+		return 	jdbcTemplate.query(sql, new RowMapper<Image>() {
+
+			public Image mapRow(ResultSet rs, int rowNum) throws SQLException { 
+				return new ImageImpl(rs.getString("image_uri"),null);
+			}
+		});
+	}
+
+	public void delete(Image image) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<Image> getImagesWithColor(String color) {
+		Color decodedColor = Color.decode(color);
+		int colorValue = colorFactory.createCompressedColor(decodedColor.getRed(), decodedColor.getGreen(), decodedColor.getBlue()).getColor();
+		String sql = "SELECT image_page.image_uri, image_page.page_uri " +
+				"FROM image_page, color_freq " +
+				"WHERE image_page.image_uri = color_freq.image_uri " +
+				"AND color = ?";
+		
+		
+		return jdbcTemplate.query(sql,new Object[] {colorValue}, new RowMapper<Image>() {
+
+			public Image mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new ImageImpl(rs.getString("image_uri"), rs.getString("page_uri"));
+			}
+			
+		});
+	}
+
+}
