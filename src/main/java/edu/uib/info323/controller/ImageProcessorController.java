f0bfa1052b9f433fcd8e7fd9b4d12c5e841068d1
EivindEE <eivind.elseth@gmail.com>
2012-05-03 13:12:16 +0200

    Updated ImageProcessorController to loop.

--- src/main/java/edu/uib/info323/controller/ImageProcessorController.java
+++ src/main/java/edu/uib/info323/controller/ImageProcessorController.java
@@ -20,56 +20,57 @@ import edu.uib.info323.model.Image;
 
 @Controller
 public class ImageProcessorController {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(ImageProcessorController.class);
 
 	@Autowired
 	ImageDao imageDao;
-	
+
 	@Autowired
 	ColorFreqDao colorFreqDao;
-	
+
 	@Autowired
 	ImageProcessor imageProcessor;
-	
+
 	File colorFreqFile = new File("src/main/resources/color_freq.sql");
-	
-	
-	
+
+
+
 	BufferedWriter writer;
-	
+
 	public ImageProcessorController() throws IOException {
 		writer = new BufferedWriter(new FileWriter(colorFreqFile,true));
 	}
-	
-	
+
+
 	@RequestMapping(value="/process")
 	public void process() throws IOException {
 		LOGGER.debug("Starting to process images.");
-		List<Image> unprocessed = imageDao.getUnprocessedImages();
-		LOGGER.debug("Images to process: "  + unprocessed.size());
-		for(Image image : unprocessed) {
-			LOGGER.debug("Starting to process ");
-			LOGGER.debug(image.getImageUri());
-			try {
-			imageProcessor.setImage(image);
-			List<ColorFreq> freqs = imageProcessor.getColorFreqs();
-			colorFreqDao.batchInsert(freqs);
-			LOGGER.debug("Finished processing, found " + freqs.size() + " colors above threshold. Starting to write SQL to file");
-			for(ColorFreq freq : freqs) {
-				writer.append("INSERT INTO COLOR_FREQ (image_uri, color, relative_freq) " +
-						"VALUES ('" + freq.getImage().getImageUri() + "'," +
-								"" + freq.getColor() + "," + freq.getRelativeFreq() + ");\n");
+		while(true) {
+			List<Image> unprocessed = imageDao.getUnprocessedImages();
+			LOGGER.debug("Images to process: "  + unprocessed.size());
+			for(Image image : unprocessed) {
+				try {
+					imageProcessor.setImage(image);
+					List<ColorFreq> freqs = imageProcessor.getColorFreqs();
+					colorFreqDao.batchInsert(freqs);
+				}
+				catch(Exception e) {
+					LOGGER.error("Got " + e.getClass() + "exception for image " + image.getImageUri() + ". Deleting image from DB");
+					try{
+						imageDao.delete(image);
+						LOGGER.debug("Image deleted");
+					}
+					catch (Exception ee) {
+						LOGGER.error("Image not deleted, got exception " + e.getClass() );
+					}
+				}
 			}
-			writer.flush();
-			LOGGER.debug("Wrote SQL to file.");
-			}
-			catch(Exception e) {
-				LOGGER.error("Got " + e.getClass() + "exception for image " + image.getImageUri() + ". Deleting image from DB");
-				imageDao.delete(image);
-			}
+			LOGGER.debug("Images Analyzed");
+			imageDao.updateAnalysedDate(unprocessed);
+			LOGGER.debug("Analyzed set");
 		}
 
 	}
-	
+
 }
