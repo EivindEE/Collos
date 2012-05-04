96e6c40c5166386afa7ccd0fbb8faf16b979adb1
EivindEE <eivind.elseth@gmail.com>
2012-05-04 15:00:09 +0200

    Changed to use ColorFreqFactory instead of calling constructor directly

--- src/main/java/edu/uib/info323/dao/rowmapper/ColorFreqRowMapper.java
+++ src/main/java/edu/uib/info323/dao/rowmapper/ColorFreqRowMapper.java
@@ -3,19 +3,25 @@ package edu.uib.info323.dao.rowmapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import edu.uib.info323.model.ColorFreq;
+import edu.uib.info323.model.ColorFreqFactory;
 import edu.uib.info323.model.ColorFreqImpl;
 import edu.uib.info323.model.Image;
+
 
 @Component
 public class ColorFreqRowMapper implements RowMapper<ColorFreq>{
 	private Image image;
+	@Autowired
+	private ColorFreqFactory freqFactory;
 	
 	public ColorFreq mapRow(ResultSet rs, int rowNum) throws SQLException {
-		return new ColorFreqImpl(image, rs.getInt("color"), rs.getInt("relative_freq"));
+		return freqFactory.createColorFreq(image, rs.getInt("color"), rs.getInt("relative_freq"));
 	}
 	public Image getImage() {
 		return image;
