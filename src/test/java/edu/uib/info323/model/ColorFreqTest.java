62b148f61bf7727f4bea33e2cf09a5dbacda3598
EivindEE <eivind.elseth@gmail.com>
2012-05-04 15:02:08 +0200

    Changed to use ColorFreqFactory

--- src/test/java/edu/uib/info323/model/ColorFreqTest.java
+++ src/test/java/edu/uib/info323/model/ColorFreqTest.java
@@ -2,6 +2,7 @@ package edu.uib.info323.model;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.uib.info323.image.test.AbstractCollosTest;
 
@@ -10,6 +11,9 @@ public class ColorFreqTest extends AbstractCollosTest {
 	private int color = 200;
 	private int relativeFreq = 5;
 	
+	@Autowired
+	private ColorFreqFactory freqFactory;
+	
 	@Before
 	public void setUp() {
 		image = new ImageImpl("imageUri", "pageUri");
@@ -17,7 +21,7 @@ public class ColorFreqTest extends AbstractCollosTest {
 
 	@Test
 	public void testContructorAndGet() {
-		ColorFreq colorFreq = new ColorFreqImpl(image, color, relativeFreq);
+		ColorFreq colorFreq = freqFactory.createColorFreq(image, color, relativeFreq);
 		assertEquals(image, colorFreq.getImage());
 		assertEquals(color, colorFreq.getColor());
 		assertEquals(relativeFreq, colorFreq.getRelativeFreq());
