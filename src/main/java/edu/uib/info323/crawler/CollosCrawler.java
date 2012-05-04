f309c037282fa5409b5a936c0a111b3cf4357202
EivindEE <eivind.elseth@gmail.com>
2012-05-04 16:03:54 +0200

    Changed to utilize ImageFactory

--- src/main/java/edu/uib/info323/crawler/CollosCrawler.java
+++ src/main/java/edu/uib/info323/crawler/CollosCrawler.java
@@ -2,6 +2,7 @@ package edu.uib.info323.crawler;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -17,6 +18,7 @@ import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 import edu.uib.info323.dao.ImageDao;
 import edu.uib.info323.model.Image;
+import edu.uib.info323.model.ImageFactory;
 import edu.uib.info323.model.ImageImpl;
 
 @Scope("prototype")
@@ -25,6 +27,8 @@ public class CollosCrawler extends WebCrawler{
 
 	@Autowired
 	private ImageDao imageDao;
+	@Autowired
+	private ImageFactory imageFactory;
 
 	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" 
 			+ "|png|tiff?|mid|mp2|mp3|mp4"
@@ -62,7 +66,7 @@ public class CollosCrawler extends WebCrawler{
 			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
 			for(WebURL webUrl : htmlParseData.getOutgoingUrls()) {
 				if(IMG_PATTERNS.matcher(webUrl.getURL()).matches()) {
-					imageList.add(new ImageImpl(webUrl.getURL(),url));
+					imageList.add(imageFactory.createImage(webUrl.getURL(),url));
 				}
 				if(imageList.size() >= 200) {
 					imageDao.batchInsert(imageList);
