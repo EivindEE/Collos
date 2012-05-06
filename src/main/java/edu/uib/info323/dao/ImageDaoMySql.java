8f8cb81d09524060a187117048fa89498ff5db32
EivindEE <eivind.elseth@gmail.com>
2012-05-06 15:56:37 +0200

    Sorted methods and fields by name

--- src/main/java/edu/uib/info323/dao/ImageDaoMySql.java
+++ src/main/java/edu/uib/info323/dao/ImageDaoMySql.java
@@ -13,8 +13,6 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -29,45 +27,27 @@ import edu.uib.info323.model.ImageFactory;
 public class ImageDaoMySql implements ImageDao{
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ImageDaoMySql.class);
-	private NamedParameterJdbcTemplate jdbcTemplate;
 	@Autowired
 	private CompressedColorFactory colorFactory;
-	
 	@Autowired
 	private ImageFactory imageFactory;
+	
+	private NamedParameterJdbcTemplate jdbcTemplate;
 
-	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-	}
-
-	public void insert(Image image) {
+	public void batchInsert(final List<Image> images) {
 		String sql = "INSERT INTO image (image_uri) VALUES (:image_uri) ON DUPLICATE KEY UPDATE image_uri = image_uri";
-		MapSqlParameterSource mapSqlParameterSource = this.getMapSqlParameterSource(image);
-		jdbcTemplate.update(sql, mapSqlParameterSource);
+		SqlParameterSource[] parameterSource = this.getSqlParameterSource(images);
+		jdbcTemplate.batchUpdate(sql, parameterSource);
 
 		sql = "INSERT INTO image_page (image_uri,page_uri) VALUES (:image_uri,:page_uri) ON DUPLICATE KEY UPDATE image_uri = image_uri";
-		jdbcTemplate.update(sql, mapSqlParameterSource);
-		
-		LOGGER.debug("Inserted " + image + " into database");
+		jdbcTemplate.batchUpdate(sql, parameterSource);
 
 	}
 
-	public Image getImageByImageUri(String imageUri) {
-		String sql = "SELECT image_uri, page_uri " +
-					 "FROM image_page " +
-					 "WHERE image_uri = :image_uri";
+	public void delete(Image image) {
+		String sql = "DELETE FROM image WHERE image_uri = :image_uri";
+		jdbcTemplate.update(sql, this.getMapSqlParameterSource(image));
 
-		Image image =  this.jdbcTemplate.query(sql, this.getMapSqlParameterSource(imageFactory.createImage(imageUri)), new ResultSetExtractor<Image>() {
-
-			public Image extractData(ResultSet rs) throws SQLException,
-			DataAccessException {
-				rs.next();
-				
-				return imageFactory.createImage(rs.getString("image_uri"), rs.getString("page_uri"));
-			}});
-		image.addPageUri(this.getPagesForImage(image));
-		return image;
 	}
 
 	public List<Image> getAllImages() {
@@ -84,41 +64,16 @@ public class ImageDaoMySql implements ImageDao{
 		return this.getPagesForImages(images);
 	}
 
-	
-
-	public void batchInsert(final List<Image> images) {
-		String sql = "INSERT INTO image (image_uri) VALUES (:image_uri) ON DUPLICATE KEY UPDATE image_uri = image_uri";
-		SqlParameterSource[] parameterSource = this.getSqlParameterSource(images);
-		jdbcTemplate.batchUpdate(sql, parameterSource);
-
-		sql = "INSERT INTO image_page (image_uri,page_uri) VALUES (:image_uri,:page_uri) ON DUPLICATE KEY UPDATE image_uri = image_uri";
-		jdbcTemplate.batchUpdate(sql, parameterSource);
-
-	}
-
-	public List<Image> getUnprocessedImages() {
-		String sql = "SELECT * FROM image WHERE date_analyzed IS NULL LIMIT 0, 100";
-
-		return 	jdbcTemplate.query(sql,new MapSqlParameterSource(), new RowMapper<Image>() {
-
-			public Image mapRow(ResultSet rs, int rowNum) throws SQLException { 
-				return imageFactory.createImage(rs.getString("image_uri"));
-			}
-		});
-	}
-	
-	public void updateAnalysedDate(final List<Image> images) {
-		String sql = "UPDATE image SET date_analyzed = :date_analyzed WHERE image_uri = :image_uri";
+	public Image getImageByImageUri(String imageUri) {
+		String sql = "SELECT page_uri " +
+					 "FROM image_page " +
+					 "WHERE image_uri = :image_uri";
+		List<String> pageUris = jdbcTemplate.queryForList(sql, new MapSqlParameterSource("image_uri", imageUri), String.class);
 		
-		jdbcTemplate.batchUpdate(sql, getSqlParameterSource(images));
-		
+		return imageFactory.createImage(imageUri, pageUris);
 	}
 
-	public void delete(Image image) {
-		String sql = "DELETE FROM image WHERE image_uri = :image_uri";
-		jdbcTemplate.update(sql, this.getMapSqlParameterSource(image));
-
-	}
+	
 
 	public List<Image> getImagesWithColor(String color) {
 		Color decodedColor = Color.decode(color);
@@ -142,14 +97,13 @@ public class ImageDaoMySql implements ImageDao{
 		LOGGER.debug("Found " + imagesWithoutPages.size() + " images");
 		return this.getPagesForImages(imagesWithoutPages);
 	}
-	
-	private List<Image> getPagesForImages(List<Image> images) {
-		LOGGER.debug("Getting the pages for the images");
-		for(Image image : images) {
-			image.addPageUri(this.getPagesForImage(image));
-		}
-		LOGGER.debug("Pages found");
-		return images;
+
+	private MapSqlParameterSource getMapSqlParameterSource(Image image) {
+		MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+		parameterSource.addValue("image_uri", image.getImageUri());
+		parameterSource.addValue("page_uri", image.getPageUris());
+		parameterSource.addValue("date_analyzed", new Date(System.currentTimeMillis()));
+		return parameterSource; 
 	}
 	
 	private List<String> getPagesForImage(Image image) {
@@ -163,6 +117,15 @@ public class ImageDaoMySql implements ImageDao{
 		return pages;
 	}
 
+	private List<Image> getPagesForImages(List<Image> images) {
+		LOGGER.debug("Getting the pages for the images");
+		for(Image image : images) {
+			image.addPageUri(this.getPagesForImage(image));
+		}
+		LOGGER.debug("Pages found");
+		return images;
+	}
+
 	private SqlParameterSource[] getSqlParameterSource(List<Image> images) {
 		List<SqlParameterSource> parameters = new ArrayList<SqlParameterSource>();
 		for(Image image : images) {
@@ -170,13 +133,40 @@ public class ImageDaoMySql implements ImageDao{
 		}
 		return parameters.toArray(new SqlParameterSource[0]);
 	}
+	
+	public List<Image> getUnprocessedImages() {
+		String sql = "SELECT * FROM image WHERE date_analyzed IS NULL LIMIT 0, 100";
+
+		return 	jdbcTemplate.query(sql,new MapSqlParameterSource(), new RowMapper<Image>() {
+
+			public Image mapRow(ResultSet rs, int rowNum) throws SQLException { 
+				return imageFactory.createImage(rs.getString("image_uri"));
+			}
+		});
+	}
+	
+	public void insert(Image image) {
+		String sql = "INSERT INTO image (image_uri) VALUES (:image_uri) ON DUPLICATE KEY UPDATE image_uri = image_uri";
+		MapSqlParameterSource mapSqlParameterSource = this.getMapSqlParameterSource(image);
+		jdbcTemplate.update(sql, mapSqlParameterSource);
+
+		sql = "INSERT INTO image_page (image_uri,page_uri) VALUES (:image_uri,:page_uri) ON DUPLICATE KEY UPDATE image_uri = image_uri";
+		jdbcTemplate.update(sql, mapSqlParameterSource);
+		
+		LOGGER.debug("Inserted " + image + " into database");
+
+	}
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+	}
 
 
-	private MapSqlParameterSource getMapSqlParameterSource(Image image) {
-		MapSqlParameterSource parameterSource = new MapSqlParameterSource();
-		parameterSource.addValue("image_uri", image.getImageUri());
-		parameterSource.addValue("page_uri", image.getPageUris());
-		parameterSource.addValue("date_analyzed", new Date(System.currentTimeMillis()));
-		return parameterSource; 
+	public void updateAnalysedDate(final List<Image> images) {
+		String sql = "UPDATE image SET date_analyzed = :date_analyzed WHERE image_uri = :image_uri";
+		
+		jdbcTemplate.batchUpdate(sql, getSqlParameterSource(images));
+		
 	}
 }
