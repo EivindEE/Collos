7c71bb833768b97de91bc39d6eaad556a86e6ba0
EivindEE <eivind.elseth@gmail.com>
2012-05-10 14:15:28 +0200

    Added methods for creating images with ids

--- src/main/java/edu/uib/info323/model/ImageFactory.java
+++ src/main/java/edu/uib/info323/model/ImageFactory.java
@@ -5,9 +5,19 @@ import java.util.List;
 public interface ImageFactory {
 	public Image createImage(String imageUri);
 	
+	public Image createImage(String imageUri, int id);
+	
 	public Image createImage(String imageUri, String pageUri);
 	
-	public Image createImage(String imageUri, List<String> pageUri);
+	public Image createImage(String imageUri, String pageUri, int id);
+	
+	public Image createImage(String imageUri, List<String> pageUris);
+	
+	public Image createImage(String imageUri, List<String> pageUris, int id);
 
 	public Image createImage(String imageUri, int height, int width);
+	
+	public Image createImage(String imageUri, int height, int width, int id);
+	
+	public Image createImage(String imageUri, List<String> pageUris, int width, int height, int id);
 }
